a053edff222ce41afc0cf92919fb574ad64035cd
maciejmirek <maciekmirek10@gmail.com>
2022-03-02 20:47:09 +0100

    Dodaj pliki projektów.

---
+++ PortalOgloszeniowy/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortalOgloszeniowy.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
